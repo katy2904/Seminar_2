adcbbf06d3ef2c41b18763c141414f5648551bc8
Katy <wc-katy@yandex.ru>
2022-08-08 21:54:50 +0700

    task 10 manual input

--- task_10/Program.cs
+++ task_10/Program.cs
@@ -6,8 +6,18 @@
 918 -> 1
 */
 
+/* вариант с рандомным числом
 int number = new Random().Next(100,1000);
 Console.WriteLine("Рандомное число: " + number);
+
+int seconddigit = number % 100 / 10;
+Console.WriteLine(seconddigit);
+*/
+
+// вариант с ручным вводом
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
 int seconddigit = number % 100 / 10;
 Console.WriteLine(seconddigit);
