e003ef8cc482b79849fb2505b4886679b44f8037
Katy <wc-katy@yandex.ru>
2022-08-11 00:34:19 +0700

    задача 13 через числа

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -5,7 +5,8 @@
 78 -> третьей цифры нет
 32679 -> 6
 */
-
+/*
+через строки:
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
@@ -15,3 +16,24 @@ else
     string num = Convert.ToString(number);  //возможно есть метод конвертации переменной, но я его пока не знаю, поэтому ввела еще одну переменную :)
     Console.WriteLine($"У числа {number} третья цифра {num[2]}");
 }
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number / 100) == 0) Console.WriteLine($"У числа {number} нет третьей цифры");
+
+int count = 0;
+int num = number;
+
+while (num > 0)
+{
+    count++;
+    num /= 10;
+}
+ 
+int div = Convert.ToInt32(Math.Pow(10, (count - 3)));
+
+int digit = number / div % 10;
+
+Console.Write($"у числа {number} третья цифра равна {digit}");
